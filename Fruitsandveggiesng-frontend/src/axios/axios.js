f95aa80b5cfc3e1e4b44d2ac794d6dbf9a8095fb
Tejasvini <tejasvini@convint.ai>
2023-01-06 08:17:48 +0000

    Add workflow file

--- Fruitsandveggiesng-frontend/src/axios/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const instance = axios.create({
-    // baseURL: "http://localhost:5656"
-    baseURL: "https://fruitsandveggies.herokuapp.com"
-});
-export default instance;
